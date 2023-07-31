dc4a23b67bd7c03179d677e6a9766bf74379a950
Elia Hayashishita <hayel537@student.otago.ac.nz>
2023-07-31 16:39:52 +1200

    finished Lab6

---
+++ Lab6/card-game/src/Card.jsx
@@ -0,0 +1,11 @@
+import Card from './Card';
+
+// props is how we pass data to components
+function Card(props) {
+
+    // this component just returns an <img />
+    return (
+        <img className='card' src={props.card.img} alt='' />
+    );
+}
+export default Card
