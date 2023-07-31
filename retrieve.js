4d7207f17b0310dbc3525bae6d5b8d28cac4c794
Elia Hayashishita <hayel537@student.otago.ac.nz>
2023-07-31 13:42:42 +1200

    Finished Lab5

--- retrieve.js
+++ retrieve.js
@@ -1,6 +1,7 @@
 //const fetch = require('node-fetch');
 
- let URL = "https://cosc203.cspages.otago.ac.nz/labs/files/emoji.json";
+ //let URL = "https://cosc203.cspages.otago.ac.nz/labs/files/emoji.json";
+ let URL = "http://oucs1555.staff.uod.otago.ac.nz:8080/messages.json";
  function response_callback(response) {
     if (response.status !== 200){
         return;
@@ -11,9 +12,9 @@
  function data_callback(data) {
      let emoji_array = JSON.parse(data);
      for(let e of emoji_array){
-        if(e.name.includes("smiling")){
-            console.log(e.name + e.emoji)
-        }
+        //if(e.name.includes("smiling")){
+            console.log(e);
+        //}
      }
  }
  fetch(URL).then(response_callback).then(data_callback);
